30e09f89bd8d119b31331f7300e2f9b8bf97ceb3
Christian Dudar <christiandudar@gmail.com>
2017-03-05 06:27:41 -0700

    Implemented Bill Splitter

--- Waitless/BillSplitter.xaml.cs
+++ Waitless/BillSplitter.xaml.cs
@@ -10,24 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Waitless
 {
     /// <summary>
-    /// Interaction logic for MenuPage.xaml
+    /// Interaction logic for BillSplitter.xaml
     /// </summary>
-    public partial class MenuPage : Page
+    public partial class BillSplitter : Window
     {
-        public MenuPage()
+        public BillSplitter()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-
         }
     }
 }
