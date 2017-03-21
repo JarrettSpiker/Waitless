18fe6e12ca5ecc8bc8e80bc02808d3094913ca8e
Jarrett Spiker <jspiker@shaw.ca>
2017-03-21 14:58:08 -0600

    Dynamically load cheque page

--- Waitless/ChequePage.xaml.cs
+++ Waitless/ChequePage.xaml.cs
@@ -1,17 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Collections.Generic;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System;
+using Waitless.model;
+using Waitless.controls;
 
 namespace Waitless
 {
@@ -20,9 +11,104 @@ namespace Waitless
     /// </summary>
     public partial class ChequePage : Page
     {
+        List<OrderedItem> pendingItems;
+        List<Tuple<OrderedItem,double>> confirmedItems;
+        List<OrderedItem> othersItems;
+
         public ChequePage()
         {
             InitializeComponent();
+            pendingItems = new List<OrderedItem>();
+            confirmedItems = new List<Tuple<OrderedItem,double>>();
+            othersItems = new List<OrderedItem>();
+
+            //init fixed "others" items
+
+            othersItems.Add(new OrderedItem(ItemDefinitionController.itemDefinitions["Rickards Red"], "otherUserId"));
+            othersItems.Add(new OrderedItem(ItemDefinitionController.itemDefinitions["Classic Burger"], "otherUserId"));
+
+
+            //TODO testing code, add some hard coded values
+
+            pendingItems.Add(new OrderedItem(ItemDefinitionController.itemDefinitions["Western Burger"], "currentUserId"));
+            pendingItems.Add(new OrderedItem(ItemDefinitionController.itemDefinitions["Nachos"], "currentUserId"));
+
+            confirmedItems.Add(Tuple.Create(new OrderedItem(ItemDefinitionController.itemDefinitions["Alexander Keiths"], "currentUserId"),1.0));
+            confirmedItems.Add(Tuple.Create(new OrderedItem(ItemDefinitionController.itemDefinitions["Jalapeno Poppers"], "currentUserId"), 0.25));
+
+
+            confirmedItems.Add(Tuple.Create(new OrderedItem(ItemDefinitionController.itemDefinitions["Water"], "currentUserId"), 2.0));
+
+            RedrawItems();
+        }
+
+        void addPendingItem(OrderedItem item)
+        {
+            pendingItems.Add(item);
+            RedrawPendingItems();
+        }
+
+        private void RedrawItems()
+        {
+            RedrawPendingItems();
+            RedrawConfirmedItems();
+            RedrawOthersItems();
+        }
+
+        private void RedrawPendingItems()
+        {
+            PendingItemsComponent.Children.Clear();
+            foreach (OrderedItem item in pendingItems)
+            {
+                PendingItemControl component = new PendingItemControl();
+                component.ItemName.Inlines.Clear();
+                component.ItemName.Inlines.Add(item.itemDefinition.name);
+                component.Price.Inlines.Clear();
+                component.Price.Inlines.Add((item.itemDefinition.cost / 100.0).ToString("F"));
+
+                PendingItemsComponent.Children.Add(component);
+            }
+        }
+
+        private void RedrawConfirmedItems()
+        {
+            ConfirmedItemsComponent.Children.Clear();
+
+
+            foreach (Tuple<OrderedItem,double> item in confirmedItems)
+            {
+                ConfirmedItemControl component = new ConfirmedItemControl();
+                component.ItemName.Inlines.Clear();
+                component.ItemName.Inlines.Add(item.Item1.itemDefinition.name);
+                component.Price.Inlines.Clear();
+                component.Price.Inlines.Add((item.Item2 * item.Item1.itemDefinition.cost / 100.0).ToString("F"));
+
+                if (item.Item1.itemDefinition.freeRefills)
+                {
+                    component.ReorderButton.Content = "Refill";
+                }
+
+                component.Quantity.Inlines.Clear();
+                component.Quantity.Inlines.Add(item.Item2.ToString("0.##"));
+
+
+                ConfirmedItemsComponent.Children.Add(component);
+            }
+        }
+
+        private void RedrawOthersItems()
+        {
+            OthersItemsComponent.Children.Clear();
+            foreach(OrderedItem item in othersItems)
+            {
+                OthersItemControl component = new OthersItemControl();
+                component.ItemName.Inlines.Clear();
+                component.ItemName.Inlines.Add(item.itemDefinition.name);
+                component.Price.Inlines.Clear();                    
+                component.Price.Inlines.Add((item.itemDefinition.cost / 100.0).ToString("F"));
+                OthersItemsComponent.Children.Add(component);
+            }
+            
         }
     }
 }
---
+++ Waitless/controls/PendingItemControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Waitless.controls
+{
+    /// <summary>
+    /// Interaction logic for PendingItemControl.xaml
+    /// </summary>
+    public partial class PendingItemControl : UserControl
+    {
+        public PendingItemControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
